513baaafd23f927d67bbec059c917e76d9753f76
Aimee <aimee@edendevelopment.co.uk>
2009-08-04 23:14:31 +0100

    Add some useful links for geohashes.

--- app/models/geohash.rb
+++ app/models/geohash.rb
@@ -3,6 +3,18 @@ class Geohash < ActiveRecord::Base
   belongs_to :graticule
   belongs_to :history
   
+  def peeron_link
+    "http://irc.peeron.com/xkcd/map/map.html?date=#{date.strftime('%Y-%m-%d')}&lat=#{graticule.latitude}&long=#{graticule.longitude}&zoom=8"
+  end
+  
+  def wiki_link
+    "http://wiki.xkcd.com/geohashing/#{date.strftime('%Y-%m-%d')}_#{graticule.latitude}_#{graticule.longitude}"
+  end
+  
+  def google_link
+    "http://maps.google.com/?ie=UTF8&ll=#{lat},#{lng}&z=8&q=loc:#{lat},#{lng}"
+  end
+  
   def self.create_for_date_and_graticule(date, graticule)
     history = History.for(date, graticule.w30?)
     return nil if history.nil?
