00e82eea9e04c19c92f59138aed6a631d0f388c8
Aimee Daniells <aimee@edendevelopment.co.uk>
2009-08-04 12:59:15 +0100

    Only subscribe to graticules if not already subscribed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ActiveRecord::Base
   has_many :subscriptions, :dependent => :destroy
   has_many :graticules, :through => :subscriptions
   
+  def subscribe_to_graticule(graticule)
+    return if graticule.nil?
+    graticules << graticule unless graticules.include?(graticule)
+  end
+  
   def self.create_with_rpx(rpx_data)
     User.create!({
       :identifier => rpx_data['identifier'],
