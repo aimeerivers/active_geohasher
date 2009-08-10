490e3d5cd5d0e7cf87b8db9efc519ea332d8312c
Aimee <aimee@edendevelopment.co.uk>
2009-08-10 18:26:29 +0100

    Adding the google maps api keys since they aren't actually private.

---
+++ config/initializers/google.rb
@@ -0,0 +1,7 @@
+GOOGLE_MAPS_API_KEY = {
+  'localhost' => 'abcdefg',
+  'activegeohasher.heroku.com' => 'ABQIAAAA9zsA-9rwXu-QtUGF2h6Z5RRKl0l7SadT5xlGrLS3qCZHKFAHLRQMrxwwhCoc7G5jYzq1qnLYyRLpBg',
+  'activegeohasher.com' => 'ABQIAAAA9zsA-9rwXu-QtUGF2h6Z5RSV6WK5trLyTKbh5FRjrteYogJsZhTuBqzYZxsmzDl44zVV_O3h7WNXyA',
+  'activegeohasher.co.uk' => 'ABQIAAAA9zsA-9rwXu-QtUGF2h6Z5RQMZn1U6jEIAsX_0EXAtxU0VR3cOxSz07tUcyb_KmdqCilooIASGfNqaQ'
+}
+
