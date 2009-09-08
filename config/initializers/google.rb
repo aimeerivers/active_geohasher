28ca4328e9d5d20815311aa8d68b887b57f8c137
Aimee <aimee@edendevelopment.co.uk>
2009-09-08 20:23:08 +0100

    Include google maps keys for the www domains.

--- config/initializers/google.rb
+++ config/initializers/google.rb
@@ -2,6 +2,8 @@ GOOGLE_MAPS_API_KEY = {
   'localhost' => 'abcdefg',
   'activegeohasher.heroku.com' => 'ABQIAAAA9zsA-9rwXu-QtUGF2h6Z5RRKl0l7SadT5xlGrLS3qCZHKFAHLRQMrxwwhCoc7G5jYzq1qnLYyRLpBg',
   'activegeohasher.com' => 'ABQIAAAA9zsA-9rwXu-QtUGF2h6Z5RSV6WK5trLyTKbh5FRjrteYogJsZhTuBqzYZxsmzDl44zVV_O3h7WNXyA',
-  'activegeohasher.co.uk' => 'ABQIAAAA9zsA-9rwXu-QtUGF2h6Z5RQMZn1U6jEIAsX_0EXAtxU0VR3cOxSz07tUcyb_KmdqCilooIASGfNqaQ'
+  'activegeohasher.co.uk' => 'ABQIAAAA9zsA-9rwXu-QtUGF2h6Z5RQMZn1U6jEIAsX_0EXAtxU0VR3cOxSz07tUcyb_KmdqCilooIASGfNqaQ',
+  'www.activegeohasher.com' => 'ABQIAAAA9zsA-9rwXu-QtUGF2h6Z5RSV6WK5trLyTKbh5FRjrteYogJsZhTuBqzYZxsmzDl44zVV_O3h7WNXyA',
+  'www.activegeohasher.co.uk' => 'ABQIAAAA9zsA-9rwXu-QtUGF2h6Z5RQMZn1U6jEIAsX_0EXAtxU0VR3cOxSz07tUcyb_KmdqCilooIASGfNqaQ'
 }
 
