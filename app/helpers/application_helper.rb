eb32f77c48dcaba6f4d1038f0437ba4753d2454d
Aimee <aimee@edendevelopment.co.uk>
2009-09-17 22:24:36 +0100

    Add more info to the front page and tidy away the javascript external loading.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def sign_in_link
+    link_to 'Sign in', "https://#{ENV['RPX_REALM']}.rpxnow.com/openid/v2/signin?token_url=#{rpx_url}", :class => 'rpxnow', :onclick => 'return false;'
+  end
+  
 end
+
