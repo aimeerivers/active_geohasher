9a94dc384777f71c79c67c0d20d0a543f5aa9966
Aimee <aimee@edendevelopment.co.uk>
2009-10-08 22:21:43 +0100

    Enable entering location without javascript, provide a better method of looking up current location, and auto-subscribe to graticule when location chosen.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,22 @@ class UsersController < ApplicationController
   before_filter :login_required
   
   def location
+    if current_user.location_set?
+      @lat = current_user.lat
+      @lng = current_user.lng
+      @location_set = true
+    else
+      location = Geokit::Geocoders::MultiGeocoder.geocode(request.headers['REMOTE_ADDR'])
+      @lat = location.lat || 49
+      @lng = location.lng || 14
+      @location_set = false
+    end
   end
   
   def update_location
-    current_user.update_attributes(:lat => params[:user][:lat], :lng => params[:user][:lng])
+    lat, lng = params[:user][:lat], params[:user][:lng]
+    current_user.update_attributes(:lat => lat, :lng => lng)
+    current_user.subscribe_to_graticule(Graticule.find_or_create_by_latitude_and_longitude(lat.to_s.split('.').first, lng.to_s.split('.').first))
     
     respond_to do |format|
       format.js { render :update_location }
