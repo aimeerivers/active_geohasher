b0c4df10fc2dbabe62c59f15e0c796097ac09864
Aimee Daniells <aimee@edendevelopment.co.uk>
2009-12-24 18:36:34 +0100

    Add custom links into email.

--- app/models/custom_link.rb
+++ app/models/custom_link.rb
@@ -8,7 +8,7 @@ class CustomLink < ActiveRecord::Base
   named_scope :newest_first, :order => 'created_at DESC'
 
   def magic_url(hash)
-    return_url = url
+    return_url = url.dup
     hash.each do |key, value|
       return_url.gsub!(/\[#{key.to_s.upcase}\]/, value.to_s)
     end
