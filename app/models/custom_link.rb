f634ff3eb78ade8775b84703c2d978e67790f26f
Aimee Daniells <aimee@edendevelopment.co.uk>
2009-12-24 18:23:05 +0100

    Include custom links on the geohash page.

--- app/models/custom_link.rb
+++ app/models/custom_link.rb
@@ -7,4 +7,12 @@ class CustomLink < ActiveRecord::Base
 
   named_scope :newest_first, :order => 'created_at DESC'
 
+  def magic_url(hash)
+    return_url = url
+    hash.each do |key, value|
+      return_url.gsub!(/\[#{key.to_s.upcase}\]/, value.to_s)
+    end
+    return_url
+  end
+
 end
