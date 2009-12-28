6a9b30d2125d59cd20fd3d8bbd866223cbeb5f6c
Aimee Daniells <aimee@edendevelopment.co.uk>
2009-12-28 17:24:19 +0100

    Enable and use pretty geohash paths for predictable URLs. This also means visitors can generate geohashes for any date they wish.

--- app/controllers/geohashes_controller.rb
+++ app/controllers/geohashes_controller.rb
@@ -1,8 +1,17 @@
 class GeohashesController < ApplicationController
 
   def show
-    @geohash = Geohash.find(params[:id].to_i(36))
-    @graticule = @geohash.graticule
+    begin
+      if params.include?('date') && params.include?('latitude') && params.include?('longitude')
+        @geohash = Geohash.find_or_create(Date.parse(params[:date]), params[:latitude], params[:longitude])
+      else
+        @geohash = Geohash.find(params[:id].to_i(36))
+      end
+      @graticule = @geohash.graticule
+    rescue
+      flash[:error] = "Geohash not found, or there was a problem looking it up."
+      redirect_to root_path
+    end
   end
 
 end
