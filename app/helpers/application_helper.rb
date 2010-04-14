794988495284ce88daf90f31701640da376bfeba
aimee <aimee@aimee-macbook-mint.(none)>
2010-04-14 17:46:02 +0100

    Work in progress on internationalization: authentication and navigation.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 module ApplicationHelper
   
   def sign_in_link
-    link_to 'Sign in', "https://#{ENV['RPX_REALM']}.rpxnow.com/openid/v2/signin?token_url=#{rpx_url}", :class => 'rpxnow', :onclick => 'return false;'
+    link_to t('authentication.sign_in'), "https://#{ENV['RPX_REALM']}.rpxnow.com/openid/v2/signin?token_url=#{rpx_url}", :class => 'rpxnow', :onclick => 'return false;'
   end
 
   def friendly_date(date)
