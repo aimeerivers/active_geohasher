68cdfecd08352178a03f273578fccab84c449bcf
aimee daniells <aimee@mychores.co.uk>
2010-04-24 16:17:12 +0100

    Email title translated.

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -3,7 +3,7 @@ class Notifier < ActionMailer::Base
 
   def upcoming_geohashes(user, start_time)
     I18n.locale = user.preferred_locale
-    subject    "Geohashing adventures [#{geohashing_dates(user, start_time)}]"
+    subject    "#{I18n.t('notifier.upcoming_geohashes.geohashing_adventures')} [#{geohashing_dates(user, start_time)}]"
     recipients user.email
     from       'activegeohasher@googlemail.com'
     sent_on    Time.now
