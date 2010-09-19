b1fd822e41492f741682877ab841970da98c5d3f
aimee daniells <aimee@mychores.co.uk>
2010-09-19 22:05:07 +0100

    And anyway, what's this doing in the private bit? :)

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -5,24 +5,22 @@ class Notifier < ActionMailer::Base
     I18n.locale = user.preferred_locale
     subject    "#{I18n.t('notifier.upcoming_geohashes.geohashing_adventures')} [#{geohashing_dates(user, start_time)}]"
     recipients user.email
-    from       'activegeohasher@googlemail.com'
     sent_on    Time.now
     
     body       :user => user, :start_time => start_time
+  end
+
+  def confirmation_of_email_delivery(number_sent)
+    subject "Delivered #{number_sent} geohashing emails"
+    recipients ENV['OWNER_EMAIL']
+    sent_on Time.now
+    body :number_sent => number_sent
   end
 
   private
 
   def geohashing_dates(user, start_time)
     user.new_geohashes_since(start_time).keys.map{|date| I18n.l(date, :format => :short)}.join(', ')
-  end
-
-  def confirmation_of_email_delivery(number_sent)
-    subject "Delivered #{number_sent} geohashing emails"
-    recipients ENV['OWNER_EMAIL']
-    from 'activegeohasher@googlemail.com'
-    sent_on Time.now
-    body :number_sent => number_sent
   end
 
 end
